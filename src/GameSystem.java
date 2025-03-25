b32206790bf378a63480fc98d2f57b6655324770
Tsuki <hung5008@gmail.com>
2025-03-26 03:58:06 +0700

    Minor changes

--- src/GameSystem.java
+++ src/GameSystem.java
@@ -1,12 +1,3 @@
 public class GameSystem {
-    public static int playerAmount;
-
-
-    public GameSystem() {
-        playerAmount = 4;
-    }
-    public GameSystem(int playerAmount) {
-        GameSystem.playerAmount = playerAmount;
-    };
 
 }
